d48b923362d081f5a5885acfffcaea208f33aaba
darkllen <yankiniigor@gmail.com>
2019-04-22 12:04:48 +0300

    1

--- Practice2.13/Practice13/src/Main.java
+++ Practice2.13/Practice13/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         myList.add("sss");
         myList.add("aaa");
         myList.add("bbb");
+        myList.addByIndex(0,"ddd");
+        System.out.println(myList.getByIndex(3));
         myList.removeByIndex(1);
         myList.remove("bbb");
     }
