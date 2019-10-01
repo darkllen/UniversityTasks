23206b1a70aaa89ffbac535fbb9437dc606bb384
Ihor Yankin <yankiniigor@gmail.com>
2019-10-01 14:59:34 +0300

    1

---
+++ year2/C++/PracticeForB2/PracticeForB2/PracticeForB2.cpp
@@ -0,0 +1,28 @@
+﻿ //Developed by: Ihor Yankin
+#include <iostream>
+#include <cassert>
+
+int main()
+{	
+  
+	double a = 1.0;
+	double b = 1;
+	long count(0);
+	while (b+ a!=b) {
+		a /= 10;
+		count++;
+	}
+	std::cout << count << " in double"<< std::endl;
+	float c = 1.0;
+	float d = 1;
+	count = 0;
+	while (d +c != d) {
+		c /= 10;
+		count++;
+	}
+	std::cout << count<< " in float";
+	#ifdef _DEBUG
+	assert(2 == 0);
+	#endif
+}
+
