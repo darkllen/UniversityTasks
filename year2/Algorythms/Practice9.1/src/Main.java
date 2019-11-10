52be0f4da52f9dbc06be0942a3278796659cc9b9
Ihor <yankiniigor@gmail.com>
2019-11-11 01:06:03 +0200

    init algs practice 9.1

--- year2/Algorythms/Practice9.1/src/Main.java
+++ year2/Algorythms/Practice9.1/src/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i< t; i++){
+            int n = Integer.parseInt(bufferedReader.readLine());
+            String arr[] = new String[n];
+            for(int j = 0; j< n; j++){
+                arr[j] = bufferedReader.readLine();
+            }
+            Arrays.sort(arr);
+            String res = "YES";
+            boolean y = false;
+            for(int k = 0; k<n-1 ; k++){
+                for(int l = k+1; l<n; l++){
+                    if(arr[k].length()<=arr[l].length()){
+                        if(arr[k].equals(arr[l].substring(0,arr[k].length()))){
+                            res = "NO";
+                            y = true;
+                            break;
+                        }
+                    }
+                    if(y)break;
+                }
+            }
+            writer.write(res);
+            writer.newLine();
+        }
+        writer.flush();
+    }
 }
