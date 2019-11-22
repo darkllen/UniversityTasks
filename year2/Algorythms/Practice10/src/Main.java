fe650c94c59a2acaf18317067d20428bac86bfd2
Ihor <yankiniigor@gmail.com>
2019-11-23 01:25:17 +0200

    finish algs Practice 10

--- year2/Algorythms/Practice10/src/Main.java
+++ year2/Algorythms/Practice10/src/Main.java
@@ -1,9 +1,20 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        BST<String, Integer> bst = new BST<>();
-        bst.put("abc",0);
-        bst.put("a",0);
-        bst.put("ab",0);
-        bst.put("dc",0);
+    public static void main(String[] args) throws IOException {
+        SearchDictionary dictionary = new SearchDictionary();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("//C:/Users/yanki/OneDrive/Documents/GitHub/UniversityTasks/year2/Algorythms/Practice10/src/3.txt")));
+        while (bufferedReader.ready()){
+            String[] l = bufferedReader.readLine().replaceAll("[^a-zA-Zа-яА-Яё ]", " ").split(" ");
+            for (int i = 0; i < l.length; i++) {
+                if (l[i].length()>0)dictionary.addWord(l[i]);
+            }
+        }
+        dictionary.delWord("как");
+        dictionary.addWord("какогог");
+
+        Iterable<String> i = dictionary.query("кол*");
+        i.forEach(System.out::println);
+
     }
 }
--- year2/Algorythms/Practice10/src/SearchDictionary.java
+++ year2/Algorythms/Practice10/src/SearchDictionary.java
@@ -1,46 +1,169 @@
-@SuppressWarnings("ALL")
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class SearchDictionary {
-    private class Node{
-        String key;
-        int value;
-        Node l;
-        Node r;
+    int count = 0;
+    static class TrieNode {
+        Map<Character, TrieNode> children = new TreeMap<Character, TrieNode>();
+        boolean leaf;
+        int countl = 0;
 
-        public Node(String key, int value) {
-            this.key = key;
-            this.value = value;
+
+        public TrieNode(int countl) {
+            this.countl = countl;
         }
     }
 
-    Node root;
+    TrieNode root = new TrieNode(0);
 
-    public void addWord(String w){
-        if (root==null){
-            root = new Node(w, 1);
+    public void addWord(String s) {
+        if(hasWord(s))return;
+        TrieNode v = root;
+        for (char ch : s.toLowerCase().toCharArray()) {
+            v.leaf = false;
+            if (!v.children.containsKey(ch)) {
+                v.children.put(ch, new TrieNode(0));
+            }
+            v = v.children.get(ch);
+            v.countl++;
         }
-        else if(w.compareTo(root.key)>0) {
-            if(root.r!=null)
-                addWord(w, root.r);
-            else root.r = new Node(w, 1);
-        } else {
-            if (root.l!=null)
-                addWord(w, root.l);
-            else root.l = new Node(w, 1);
-        }
+        if(v.children.size()<=0)
+        v.leaf = true;
+        count++;
     }
 
-    public void addWord(String w, Node root){
-         if(w.compareTo(root.key)>0) {
-            if(root.r!=null)
-            addWord(w, root.r);
-            else root.r = new Node(w, 1);
-        } else {
-            if (root.l!=null)
-            addWord(w, root.l);
-            else root.l = new Node(w, 1);
+    public boolean hasWord(String s) {
+        TrieNode v = root;
+        for (char ch : s.toLowerCase().toCharArray()) {
+            if (!v.children.containsKey(ch)) {
+                return false;
+            } else {
+                v = v.children.get(ch);
+            }
         }
+        AtomicBoolean t = new AtomicBoolean(true);
+        if(!v.leaf){
+            int c = v.countl;
+            v.children.forEach((x,y)->{
+                if (y.countl==c) t.set(false);
+            });
+        }
+        return t.get();
+    }
+
+    public boolean delWord(String s) {
+        if(!hasWord(s))return false;
+        TrieNode v = root;
+        for (char ch : s.toLowerCase().toCharArray()) {
+                if(v.children.get(ch).leaf){
+                    v.children.remove(ch);
+                    count--;
+                    return true;
+                }
+                v = v.children.get(ch);
+                v.countl--;
+        }
+        count--;
+        return true;
     }
 
 
+    private void cr(ArrayList<String> res, TrieNode v, String curr){
+        v.children.forEach((x,y)->{
+            if(y.leaf){
+                res.add(curr+x);
+            } else{
+                AtomicReference<Boolean> a = new AtomicReference<>(false);
+                AtomicInteger sum = new AtomicInteger();
+                y.children.forEach((m,n)->{
+                     sum.addAndGet(n.countl);
+                });
+                if(sum.get()!=y.countl)res.add(curr+x);
+            }
+            cr(res,y, curr+x);
+        });
+    }
+    public Iterable<String> query(String query){
+        TrieNode v = root;
+        ArrayList<String> res = new ArrayList();
+        if(query.endsWith("*")){
+            for(int i = 0; i< query.length()-1; i++){
+                if (!v.children.containsKey(query.charAt(i))) {
+                    return null;
+                } else {
+                    v = v.children.get(query.charAt(i));
+                }
+            }
+            String curr = query.substring(0,query.length()-1);
+            cr(res,v,curr);
+
+        }else {
+            for(int i = 0; i< query.length(); i++){
+                if (!v.children.containsKey(query.charAt(i))) {
+                    return null;
+                } else {
+                    v = v.children.get(query.charAt(i));
+                }
+            }
+        }
+        if(query.endsWith("*")) {
+            res.add(query.substring(0,query.length()-1));
+            if(!v.leaf){
+                int c = v.countl;
+                v.children.forEach((x,y)->{
+                    if (y.countl==c)res.remove(query.substring(0,query.length()-1));
+                });
+            }
+        }else {
+            res.add(query);
+            if(!v.leaf){
+                int c = v.countl;
+                v.children.forEach((x,y)->{
+                    if (y.countl==c)res.remove(query);
+                });
+            }
+        }
+
+
+        Collections.sort(res);
+        return res;
+    }
+
+    public int countWords(){
+        return count;
+    }
+
+
+
+    static Map<Integer,String> levelSpacesMap = new HashMap<Integer,String>();
+
+    static String getSpace(int level) {
+        String result = levelSpacesMap.get(level);
+        if (result == null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i<level; i++) {
+                sb.append(" ");
+            }
+            result = sb.toString();
+            levelSpacesMap.put(level,result);
+        }
+        return result;
+    }
+
+    public static void printSorted(TrieNode node) {
+        printSorted2(node,0);
+    }
+
+    private static void printSorted2(TrieNode node, int level) {
+        for (Character ch : node.children.keySet()) {
+            System.out.println(getSpace(level)+ch);
+            printSorted2(node.children.get(ch), level+1);
+        }
+        if (node.leaf) {
+            System.out.println();
+        }
+    }
 
 }
