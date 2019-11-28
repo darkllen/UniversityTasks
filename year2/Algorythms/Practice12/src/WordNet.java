d5c19e21b1de6679111a30e871581b29b5bb616d
Ihor <yankiniigor@gmail.com>
2019-11-28 09:29:58 +0200

    init algs Practice12 and add categoryInfo NIT

---
+++ year2/Algorythms/Practice12/src/WordNet.java
@@ -0,0 +1,28 @@
+public class WordNet {
+    Digraph digraph;
+    public WordNet(String synsets, String hypernyms){
+
+    }
+    // множина іменників, що повертається як ітератор (без дублікатів)
+    public Iterable<String> nouns(){
+        return null;
+    }
+    // чи є слово серед WordNet іменників?
+    public boolean isNoun(String word){
+        return false;
+    }
+    // відстань між nounA і nounB
+    public int distance(String nounA, String nounB){
+        return 0;
+    }
+    // синсет що є спільним предком nounA і nounB
+// в найкоршому шляху до предка
+    public String sap(String nounA, String nounB){
+        return null;
+    }
+    // тестування
+    public static void main(String[] args){
+
+    }
+
+}
